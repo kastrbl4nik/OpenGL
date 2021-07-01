a913f2027d5e5d2ec37ca4271ad95ead04bbedda
Shunko Nickeata <shunjon496@gmail.com>
2021-07-01 19:09:31 +0200

    Added math library

--- src/Application.cpp
+++ src/Application.cpp
@@ -14,6 +14,9 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -56,17 +59,26 @@ int main(void)
             2, 3, 0,
         };
 
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
         VertexArray va;
         VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+
         VertexBufferLayout layout;
         layout.Push<float>(2);
         layout.Push<float>(2);
+
         va.AddBuffer(vb, layout);
+
         IndexBuffer ib(indices, 6);
+
+        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+        shader.SetUniformMat4f("u_MVP", proj);
 
         Texture texture("res/textures/vitalik.png");
         texture.Bind();
--- src/Shader.h
+++ src/Shader.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "glm/glm.hpp"
+
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -22,6 +24,7 @@ public:
 
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 	void SetUniform1i(const std::string& name, int value);
+	void SetUniformMat4f(const std::string& name, const glm::mat4 matrix);
 private:
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int GetUniformLocation(const std::string name);
